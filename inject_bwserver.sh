53397c5e623759128e372acafc263015afcf809f
sycured <60801403+sycured@users.noreply.github.com>
2020-06-13 19:17:28 -0500

    dedup via function

--- inject_bwserver.sh
+++ inject_bwserver.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-faust -A bwserver send @compute '{"nblisteners":120, "bitrate":64.8}'
-faust -A bwserver send @compute '{"nblisteners":10, "bitrate":256.1}'
-faust -A bwserver send @compute '{"nblisteners":55, "bitrate":96.8}'
-faust -A bwserver send @compute '{"nblisteners":432, "bitrate":512}'
-faust -A bwserver send @compute '{"nblisteners":32, "bitrate":384}'
-faust -A bwserver send @compute '{"nblisteners":1, "bitrate":1048}'
-faust -A bwserver send @compute '{"nblisteners":74, "bitrate":1025.8}'
-faust -A bwserver send @compute '{"nblisteners":42, "bitrate":2056}'
-faust -A bwserver send @compute '{"nblisteners":432, "bitrate":9082.4}'
-faust -A bwserver send @compute '{"nblisteners":65465, "bitrate":750.3}'
-faust -A bwserver send @compute '{"nblisteners":3132, "bitrate":432.6}'
-faust -A bwserver send @compute '{"nblisteners":121, "bitrate":43243.77}'
+inject(){
+  faust -A bwserver send @compute "$1"
+}
+
+inject '{"nblisteners":120, "bitrate":64.8}'
+inject '{"nblisteners":10, "bitrate":256.1}'
+inject '{"nblisteners":55, "bitrate":96.8}'
+inject '{"nblisteners":432, "bitrate":512}'
+inject '{"nblisteners":32, "bitrate":384}'
+inject '{"nblisteners":1, "bitrate":1048}'
+inject '{"nblisteners":74, "bitrate":1025.8}'
+inject '{"nblisteners":42, "bitrate":2056}'
+inject '{"nblisteners":432, "bitrate":9082.4}'
+inject '{"nblisteners":65465, "bitrate":750.3}'
+inject '{"nblisteners":3132, "bitrate":432.6}'
+inject '{"nblisteners":121, "bitrate":43243.77}'
